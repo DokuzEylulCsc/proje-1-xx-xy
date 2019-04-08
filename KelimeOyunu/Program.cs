c6c97f43393c06c58fa17b3392a153a48e2c4226
Süheyla Çimen <suheylacimens@gmail.com>
2019-04-08 21:19:12 +0300

    Kaynakça

--- KelimeOyunu/Program.cs
+++ KelimeOyunu/Program.cs
@@ -4,6 +4,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+/*KAYNAKÇA 
+ * https://www.yazilimkodlama.com/programlama/c-txt-dosyasi-okuma-ve-turkce-karakter-sorunu-cozumu/
+ * https://www.kodlamamerkezi.com/c-net/c-ile-dosya-okuma-ve-yazma-islemleri/
+ * https://www.youtube.com/user/mxsoysal
+ * https://www.udemy.com/c-sharp-kursu/learn/v4/t/lecture/8921152?start=0
+ */
 namespace KelimeOyunu
 {
     class Program
